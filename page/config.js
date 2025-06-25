8d31b4f162f146daaac175cc611f4d9d75bd4de0
Automast <54687228+Automast@users.noreply.github.com>
2025-06-25 20:31:46 +0100

    Add files via upload

---
+++ page/config.js
@@ -0,0 +1,13 @@
+// config.js
+const config = {
+  pageTitle: "Support Baby Jhon's Urgent Heart Surgery",
+  donorName: "boy Williams",
+  donorNameAgain: "boy Williams",
+  role: "Single Mother",
+  imageFile: "jhon.png",
+  totalGoal: 100000,
+  totalDonorsAt100: 2930,
+  donationPercentage: 47,
+  description: "My name is Grace and I am writing this with a broken heart. My baby boy Jhon has been in the ICU since the day he was born. He was diagnosed with a rare and severe heart condition called \"Transposition of the Great Arteries, Pulmonary Valve Stenosis, and a Ventricular Septal Defect (VSD).\" The doctors have told us that this condition is incompatible with life unless he undergoes immediate surgery. 🥺 Since the moment he was born, my husband and I have watched our child fight for every breath. We left everything behind in our small town to be here with him in Boston, where he’s been hooked up to machines, struggling to survive. The pain of seeing him fight this battle, so small and helpless, is unbearable. My heart shatters every time I look at him. He needs two urgent surgeries right here in Boston to survive the next few months. After that, he will need a final surgery in New York. The cost of everything is overwhelming, and we can’t do this alone. I’m begging for your help. Every day, I watch my baby boy fight for his life, but I know the odds are against us. He’s so young, and yet he’s already had to endure more than any child should ever have to go through. But we won’t give up on him, and we can’t let him suffer any longer. Please, I’m asking for your kindness and generosity to help save my son’s life. The surgeries, the medical bills, the care—everything is too much for us to carry on our own. Any donation, no matter how small, can help give him a chance at a future. He deserves that chance. He deserves a life full of love, joy, and happiness. I don’t know how much time we have left, but I will fight for my son with every ounce of strength I have. Please, help us. Together, we can give him the chance to live the life he deserves. Thank you for your kindness, your prayers, and your support. ❤️",
+  defaultLink: "/index.html"
+};
